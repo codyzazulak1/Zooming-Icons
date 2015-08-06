bda3cf2a2d25c00f837e26210376dc42685575f3
codyzazulak1 <codyzazulak1@gmail.com>
2015-08-06 23:25:05 +0000

    couldnt get layout corrent

---
+++ Zooming Icons/MenuCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MenuCollectionViewCell.swift
+//  Zooming Icons
+//
+//  Created by Cody Zazulak on 2015-08-06.
+//  Copyright (c) 2015 Cody Zazulak. All rights reserved.
+//
+
+import UIKit
+
+class MenuCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var viewColour: UIView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
---
+++ Zooming Icons/MenuViewController.swift
@@ -0,0 +1,99 @@
+//
+//  MenuViewController.swift
+//  Zooming Icons
+//
+//  Created by Cody Zazulak on 2015-08-06.
+//  Copyright (c) 2015 Cody Zazulak. All rights reserved.
+//
+
+import UIKit
+
+let reuseIdentifier = "Cell"
+
+class MenuViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        collectionView?.contentInset.top = 80
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        //#warning Incomplete method implementation -- Return the number of sections
+        return 2
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+        if section == 0 {
+            return UIEdgeInsets(top: 0,left: 0,bottom: 0,right: 0)
+        } else  {
+            return UIEdgeInsets(top: 0,left: 0,bottom: 0,right: 0)
+        }
+    }
+
+
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if section == 0 {
+            return 2
+        } else  {
+            return 3
+        }
+        
+    }
+
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! MenuCollectionViewCell
+        cell.layer.cornerRadius = cell.frame.size.width/2
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(collectionView: UICollectionView, shouldHighlightItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(collectionView: UICollectionView, shouldSelectItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(collectionView: UICollectionView, shouldShowMenuForItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(collectionView: UICollectionView, canPerformAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) -> Bool {
+        return false
+    }
+
+    override func collectionView(collectionView: UICollectionView, performAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) {
+    
+    }
+    */
+
+}
